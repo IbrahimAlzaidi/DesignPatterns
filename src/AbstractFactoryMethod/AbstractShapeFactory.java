0bbe507a85ce803b67ba643ffee4ff1ec07732b1
ibalzaidi <iaz@hippodata.net>
2022-09-14 17:07:40 +0200

    Refactor AbstractShapeFactory to new file.

--- src/AbstractFactoryMethod/AbstractShapeFactory.java
+++ src/AbstractFactoryMethod/AbstractShapeFactory.java
@@ -21,22 +21,3 @@ class ShapeFactory extends AbstractShapeFactory {
 
 }
 
-abstract class AbstractColorFactory {
-    abstract Color getColor(String color);
-}
-class ColorFactory extends AbstractColorFactory{
-
-    @Override
-    Color getColor(String color) {
-        if (color == null)
-            return null;
-        else if (color.equalsIgnoreCase("RED"))
-            return new Red();
-        else if (color.equalsIgnoreCase("GREEN"))
-            return new Green();
-        else if (color.equalsIgnoreCase("BLUE"))
-            return new Blue();
-
-        return null;
-    }
-}
