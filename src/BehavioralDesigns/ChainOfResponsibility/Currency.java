56b6e7e5a9c040a85c365ba938f924562ce05586
ibalzaidi <iaz@hippodata.net>
2022-09-27 11:39:57 +0200

    Implement ChainOfResponsibility Pattern.

---
+++ src/BehavioralDesigns/ChainOfResponsibility/Currency.java
@@ -0,0 +1,13 @@
+package BehavioralDesigns.ChainOfResponsibility;
+
+public class Currency {
+    private int amount;
+
+    public Currency(int amt){
+        amount = amt;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
